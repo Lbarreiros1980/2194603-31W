5200b51c360bc6e34a2f4241f38fa47fc20e292a
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2022-11-02 14:40:48 -0400

    c10 navigation du carrousel

--- wp-content/plugins/lb_carroussel/lb_carrousel.php
+++ wp-content/plugins/lb_carroussel/lb_carrousel.php
@@ -14,20 +14,51 @@ Author: Lidia Barreiros
 Author URI: https://github.com/Lbarreiros1980
 */
 
- 
+// filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
+// plugin_dir_path() // retourne le chemin du répertoire du plugin
+// __FILE__ // le fichier en train de s'exécuter
+// wp_enqueue_style() // Intègre le link:css dans la page
+// wp_enqueue_script() // intègre le script dans la page
+// wp_enqueue_scripts // le hoo
+//
 
 
-function genere_html(){
+
+function em_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+wp_enqueue_style('em_carrousel_css',
+                plugin_dir_url(__FILE__) . "style.css",
+                array(),
+                $version_css,
+                false);
+
+
+wp_enqueue_script('em_carrousel_js',
+                plugin_dir_url(__FILE__) . "js/carrousel.js",
+                array(),
+                $version_js,
+                true);                
+}
+
+add_action("wp_enqueue_scripts","em_enqueue");
+
+
+
+function lb_boite_carrousel(){
     /////////////////////////////////////// HTML
     // Le conteneur d'une boîte
     $contenu = 
-    
-    "<div class='boite'>"
-    . "<code>----------Auteur: " . get_the_author() . "</code>"
-    . "<date>----------Date de publication: " . get_the_date() . "</date>"
-    . "<code>----------Adresse URL" . get_the_guid() . "</code>"
-    . '</div> <!-- fin class="boite" -->';
+    "<button class='bouton'>Ouvrir carrousel</button>"
+     .'<div class="carrousel">'
+        .'<button class="carrousel__x">X</button>'
+        .'<figure class="carrousel__figure"></figure>'
+        .'<form class="carrousel__form"></form>'
+    . '</div> <!-- fin class="carrousel" -->';
     
     return $contenu;
    }
-   add_shortcode('lb_carrousel', 'genere_html');
+
+   add_shortcode('lb_carrousel', 'lb_boite_carrousel');
