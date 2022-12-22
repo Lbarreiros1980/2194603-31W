139eae0b51ee1b843fef0735c35d15de5f876a6f
LB <88776130+Lbarreiros1980@users.noreply.github.com>
2022-12-22 18:19:40 -0500

    Add files via upload
    ajout du fichier sass

--- index.php
+++ index.php
@@ -1,17 +1,46 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package igc31w
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+get_header();
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+?>
+	<main class="site__main">
+		<code>index.php</code>
+		<?php
+		if ( have_posts() ) :
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post(); ?>
+			<h1><?= get_the_title(); ?></h1>
+
+			<?php the_content();
+			$le_permalien = "<a href='" . get_the_permalink() . "'>Suite</a>";
+			?>
+			
+			<h2><?= get_the_title(); ?></h2>
+			<blockquote><?= wp_trim_words(get_the_excerpt(),5, $le_permalien); ?></blockquote>
+			
+			<pre><?php the_category(); ?></pre>
+			<pre><?php the_date(); ?></pre>
+			<pre><?php the_permalink();  ?></pre>
+			<pre><?php the_author(); ?></pre>
+
+<?php
+			endwhile;
+			endif;	
+		?>
+	</main><!-- #main -->
+<?php
+get_footer();
+
