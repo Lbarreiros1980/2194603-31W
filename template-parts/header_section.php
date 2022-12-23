40850fd0b7114e1b9f00e72ffd4e28e175b7dd75
LB <88776130+Lbarreiros1980@users.noreply.github.com>
2022-12-22 22:16:03 -0500

    Add files via upload
    upload du template_parts

---
+++ template-parts/header_section.php
@@ -0,0 +1,10 @@
+<section class="header_section">
+
+	<div class="texte">
+		<h1><?= the_title(); ?></h1>
+		<p><?= the_content(); ?></p>
+	</div>
+
+	<img src="<?= the_field( 'logo_img' ); ?>" alt="logo du Collège Maisonneuve">
+
+</section>
